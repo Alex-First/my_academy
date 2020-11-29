a9fd55a593a61e109bb9548f4f8efb5ed364c9a4
FirstSacha <xelader2011@yahoo.com>
2020-11-29 13:48:11 +0200

    Home Work 3 Fragments

--- app/src/main/java/academy/alexfirst/app/MainActivity.kt
+++ app/src/main/java/academy/alexfirst/app/MainActivity.kt
@@ -1,23 +1,32 @@
 package academy.alexfirst.app
 
-import android.animation.ObjectAnimator
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import academy.alexfirst.app.fragments.FragmentMoviesDetails
+import academy.alexfirst.app.fragments.FragmentMoviesList
 import android.os.Bundle
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val fragmentMoviesList = FragmentMoviesList()
+    private val fragmentMoviesDetails = FragmentMoviesDetails()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        moveToMovieDetailsActivity()
+        if (savedInstanceState == null) {
+            showFragment(fragmentMoviesList)
+        }
+
     }
 
-    fun moveToMovieDetailsActivity(){
-        val intent = Intent(this, MovieDetailsActivity::class.java)
-        startActivity(intent)
+    fun showFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction()
+                .apply {
+                    add(R.id.fragments_container, fragment)
+                    commit()
+                }
     }
-
 
 }
