6195304ed26d577f1f71e55379232f8f21833d81
FirstSacha <xelader2011@yahoo.com>
2020-12-04 17:43:22 +0200

    Home Work 4 Recycler

--- app/src/main/java/academy/alexfirst/app/CustomAdapter.kt
+++ app/src/main/java/academy/alexfirst/app/CustomAdapter.kt
@@ -1,34 +1,42 @@
 package academy.alexfirst.app
 
+import academy.alexfirst.app.models.Actor
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.android.fundamentals.data.models.Move
 
-class CustomAdapter: RecyclerView.Adapter<ActorsViewHolder>(){
+class CustomAdapter: RecyclerView.Adapter<ActorViewHolder>(){
 
-    private var moves: List<Move> = listOf()
+    private var actors = listOf<Actor>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActorsViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActorViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val view = layoutInflater
                 .inflate(R.layout.view_holder_actor, parent, false)
-        return ActorsViewHolder(view)
+        return ActorViewHolder(view)
 
     }
 
-    override fun onBindViewHolder(holder: ActorsViewHolder, position: Int) {
-       // holder.onBindViewHolder(moves[position])
+    override fun onBindViewHolder(holder: ActorViewHolder, position: Int) {
+        holder.onBindViewHolder(actors[position])
 
     }
 
-    override fun getItemCount() =  moves.size
+    override fun getItemCount() =  actors.size
 
 }
 
-abstract class ActorsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+class ActorViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-/*private val RecyclerView.ViewHolder.context
-    get() = this.itemView.context*/
+    private val image: ImageView = itemView.findViewById(R.id.imageView_actor)
+    private val name: TextView = itemView.findViewById(R.id.textView_name)
+
+    fun onBindViewHolder(actor: Actor) {
+        image.setImageResource(actor.image)
+        name.text = actor.name
+    }
+}
 
---
+++ app/src/main/java/academy/alexfirst/app/models/Actor.kt
@@ -0,0 +1,6 @@
+package academy.alexfirst.app.models
+
+data class Actor(
+        val image: Int,
+        val name: String
+)
